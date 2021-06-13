e969456a0f57184b0b27c2b6d6886b8efd21a978
Jailson Cavalcanti <jailsonmc@gmail.com>
2021-06-13 21:21:36 +0100

    backend initializr

---
+++ backend/src/main/java/com/jailsonmc/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.jailsonmc.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
