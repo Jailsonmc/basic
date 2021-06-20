73902842223908870fba8d178e4bb2615e83f375
Jailson Cavalcanti <jailsonmc@gmail.com>
2021-06-20 22:27:08 +0100

    Define Routes for Angular AppRoutingModule

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddHelloWorldComponent } from './components/add-hello-world/add-hello-world.component';
+import { HelloWorldDetailsComponent } from './components/hello-world-details/hello-world-details.component';
+import { HelloWorldListComponent } from './components/hello-world-list/hello-world-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'hello-worlds', pathMatch: 'full'},
+  {path: 'hello-worlds', component: HelloWorldListComponent},
+  {path: 'hello-worlds/:id', component: HelloWorldDetailsComponent},
+  {path: 'add', component: AddHelloWorldComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
